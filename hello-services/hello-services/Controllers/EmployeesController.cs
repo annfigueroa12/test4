ca3f8772dc7d8aa33083335f0a2b32fe5876375c
Burke Holland <burkeholland@gmail.com>
2012-07-17 09:49:26 -0500

    Update WebAPI Nuget package to Microsoft.AspNet.WebApi

--- hello-services/hello-services/Controllers/EmployeesController.cs
+++ hello-services/hello-services/Controllers/EmployeesController.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ServiceModel;
-using System.ServiceModel.Web;
 using System.Web.Http;
 
 namespace hello_services.Controllers {
